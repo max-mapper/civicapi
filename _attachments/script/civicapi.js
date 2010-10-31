d839454d9ce1cd9827306e83c1e84814f04bc42c
Max Ogden <max@maxogden.com>
2010-10-30 22:19:13 -0700

    load a dataset on page load to not confuse people

--- _attachments/script/civicapi.js
+++ _attachments/script/civicapi.js
@@ -1,4 +1,5 @@
-var map, po, geoJson, elements, selectedElement, datasets, currentFeatures, couchUrl = "nycapi.couchone.com", currentDataset = "parks";
+var map, po, geoJson, elements, selectedElement, datasets, currentFeatures, couchUrl = "nycapi.couchone.com", 
+currentDataset = "oem_hospitals";
 function getDbs() {
   $.ajax({
     url: "http://" + couchUrl + "/nycapi/_design/civicapi/_view/datasets",
@@ -42,128 +43,128 @@ $(function(){
   map = po.map()
       .container(document.getElementById("map").appendChild(po.svg("svg")))
       .center({lat: 40.7143528, lon: -74.0059731})
-      .zoom(16)
+      .zoom(14)
       .zoomRange([0, 20])
       .add(po.interact());
 
-      map.add(po.image()
-          .url(po.url("http://{S}tile.cloudmade.com"
-          + "/675b7177e2e14f2c85edf6455ad5cea4" // http://cloudmade.com/register
-          + "/24968/256/{Z}/{X}/{Y}.png")
-          .hosts(["a.", "b.", "c.", ""])));
-      
-      geoJson = po.geoJson();
-      geoJson.on("load", function(e){ elements = e.features });
-      
-    	geoJson.container()
-    		.addEventListener("mouseover", onMouseOver, false);		
+  map.add(po.image()
+      .url(po.url("http://{S}tile.cloudmade.com"
+      + "/675b7177e2e14f2c85edf6455ad5cea4" // http://cloudmade.com/register
+      + "/24968/256/{Z}/{X}/{Y}.png")
+      .hosts(["a.", "b.", "c.", ""])));
+  
+  geoJson = po.geoJson();
+  geoJson.on("load", function(e){ elements = e.features });
+  
+	geoJson.container()
+		.addEventListener("mouseover", onMouseOver, false);		
 
-    	geoJson.container()
-    		.addEventListener("mouseout", onMouseOut, false);
+	geoJson.container()
+		.addEventListener("mouseout", onMouseOut, false);
 
-    	geoJson.container()
-    		.addEventListener("click", onMouseClick, false);
-             
-      showDataset(currentDataset);
-              
-      map.add(po.compass()
-          .pan("none"));
+	geoJson.container()
+		.addEventListener("click", onMouseClick, false);
+         
+  showDataset(currentDataset);
+          
+  map.add(po.compass()
+      .pan("none"));
 
-      function dataFor(element) {
-        var match;
-        $.each(elements, function(i,el) {
-          if (el.element == element) {
-            match = el;
-            return;
-          }
-        });
-        return match;
+  function dataFor(element) {
+    var match;
+    $.each(elements, function(i,el) {
+      if (el.element == element) {
+        match = el;
+        return;
       }
-    
-      function onMouseClick(e) {
-      	if (selectedElement) {
-      		selectedElement.setAttribute("class", "hover");
-      	}
+    });
+    return match;
+  }
 
-      	e.target.setAttribute("class", "selected");
+  function onMouseClick(e) {
+  	if (selectedElement) {
+  		selectedElement.setAttribute("class", "hover");
+  	}
 
-      	selectedElement = e.target;
-      	fetchMetadata(dataFor(e.target).data.properties.couchID);
-      }
+  	e.target.setAttribute("class", "selected");
 
-      function onMouseOver(e) {
-    	  if (e.target != selectedElement) e.target.setAttribute("class", "hover");
-    	  updateHoverWindow();
-      }
-      
-      function onMouseOut(e) {
-        if (e.target != selectedElement) e.target.setAttribute("class", "");  
-        hideHoverWindow();     
-      }
+  	selectedElement = e.target;
+  	fetchMetadata(dataFor(e.target).data.properties.couchID);
+  }
 
-      function initializeUI() {
-      	initializeInfoWindow();
-      	hideHoverWindow();
-      	$(this).mousemove(onMouseMove);
-      }
-      
-      function formatMetadata(data) {
-        out = '<dl>';
-        $.each(data, function(key, val) {
-          if (typeof(val) == 'string' && key[0] != '_') {
-            out = out + '<dt>' + key + '<dd>' + val;
-          } else if (typeof(val) == 'object' && key != "geometry" && val != null) {
-            if (key == 'properties') {
-              $.each(val, function(attr, value){
-                out = out + '<dt>' + attr + '<dd>' + value;
-              })
-            } else {
-              out = out + '<dt>' + key + '<dd>' + val.join(', ');
-            }
-          }
-        });
-        out = out + '</dl>';
-        return out;
-      }
-      
-      function fetchMetadata(docid) {
-        $.ajax({
-          url: "http://" + couchUrl + "/" + currentDataset + "/" + docid,
-          dataType: 'jsonp',
-          success: function(data){
-            updateInfoWindow(data);
-          }
-        })
-      }
-      
-      function initializeInfoWindow() {
-      	$("#infoClose").click(closeInfoWindow);
-      }
+  function onMouseOver(e) {
+	  if (e.target != selectedElement) e.target.setAttribute("class", "hover");
+	  updateHoverWindow();
+  }
+  
+  function onMouseOut(e) {
+    if (e.target != selectedElement) e.target.setAttribute("class", "");  
+    hideHoverWindow();     
+  }
 
-      function updateInfoWindow(data) {
-      	$("#info").show("fast");
-      	$("#infoTitle").html('Details');
-      	$("#infoDescription").html(formatMetadata(data));
+  function initializeUI() {
+  	initializeInfoWindow();
+  	hideHoverWindow();
+  	$(this).mousemove(onMouseMove);
+  }
+  
+  function formatMetadata(data) {
+    out = '<dl>';
+    $.each(data, function(key, val) {
+      if (typeof(val) == 'string' && key[0] != '_') {
+        out = out + '<dt>' + key + '<dd>' + val;
+      } else if (typeof(val) == 'object' && key != "geometry" && val != null) {
+        if (key == 'properties') {
+          $.each(val, function(attr, value){
+            out = out + '<dt>' + attr + '<dd>' + value;
+          })
+        } else {
+          out = out + '<dt>' + key + '<dd>' + val.join(', ');
+        }
       }
-
-      function closeInfoWindow() {
-      	$("#info").hide("fast");
-      	selectedElement.setAttribute("class", "");  
-      	selectedElement = null;
+    });
+    out = out + '</dl>';
+    return out;
+  }
+  
+  function fetchMetadata(docid) {
+    $.ajax({
+      url: "http://" + couchUrl + "/" + currentDataset + "/" + docid,
+      dataType: 'jsonp',
+      success: function(data){
+        updateInfoWindow(data);
       }
+    })
+  }
+  
+  function initializeInfoWindow() {
+  	$("#infoClose").click(closeInfoWindow);
+  }
 
-      function updateHoverWindow() {
-      	$("#hoverLabel").show().html("View more info");
-      }
+  function updateInfoWindow(data) {
+  	$("#info").show("fast");
+  	$("#infoTitle").html('Details');
+  	$("#infoDescription").html(formatMetadata(data));
+  }
 
-      function hideHoverWindow() {
-      	$("#hoverLabel").hide();
-      }
+  function closeInfoWindow() {
+  	$("#info").hide("fast");
+  	selectedElement.setAttribute("class", "");  
+  	selectedElement = null;
+  }
 
-      function onMouseMove(e) {
-      	$("#hoverLabel").css('top',  e.pageY + 10);
-      	$("#hoverLabel").css('left', e.pageX + 10);
-      }
+  function updateHoverWindow() {
+  	$("#hoverLabel").show().html("View more info");
+  }
 
-      initializeUI();
+  function hideHoverWindow() {
+  	$("#hoverLabel").hide();
+  }
+
+  function onMouseMove(e) {
+  	$("#hoverLabel").css('top',  e.pageY + 10);
+  	$("#hoverLabel").css('left', e.pageX + 10);
+  }
+
+  initializeUI();
 })
